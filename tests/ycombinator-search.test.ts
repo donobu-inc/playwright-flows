2d28c64c41db7b124779d63bbe1f908a610f6845
Vasusen Patil <vasusen@gmail.com>
2025-08-21 15:53:40 -0700

    updating the search test

--- tests/ycombinator-search.test.ts
+++ tests/ycombinator-search.test.ts
@@ -1,52 +1,35 @@
-/**
- * Be sure that Donobu is installed before running this script...
- *    'npm install donobu' or 'yarn add donobu'
- *
- * Also, be sure that Playwright's browsers are installed...
- *    'npx playwright install'
- *
- * Also, since this test is using tools that require the usage of an LLM, be
- * sure to have an appropriate LLM API key available as an environment variable
- * (e.g. OPENAI_API_KEY, ANTHROPIC_API_KEY, etc).
- */
-import { test } from "donobu";
-import { gptClientFixture } from "donobu";
+import { test } from 'donobu';
 
-const testTitle = "Test for https://news.ycombinator.com";
-const testDetails = {
-  annotation: {
-    type: "objective",
-    description: `Check the search results for YCombinator
-  1) Scroll down all the way to see the search box and submit search for "dogfooding".
-  2) Assert that the search results show "The Ultimate Dogfooding Story" in the top few links.`,
-  },
+const title = 'Test for https://news.ycombinator.com';
+const details = {
+  annotation: [
+    {
+      type: 'objective',
+      description: `Scroll down to find the search box and then do a search for "mac app for web testing".
+Assert that one of the top search results is for "Donobu".`,
+    },
+  ],
 };
-test.extend({ gptClient: gptClientFixture() })(
-  testTitle,
-  testDetails,
-  async ({ page }) => {
-    // Initializing web navigation.
-    await page.goto("https://news.ycombinator.com");
-    // Scrolling down to access the search box at the bottom of the page.
-    await page.scroll({
-      direction: "DOWN",
-    });
-    // Entering the search term 'dogfooding' to find relevant results.
-    await page.inputText({
-      text: "dogfooding",
-      finalizeWithSubmit: true,
-      selector: {
-        element: [
-          "form > :nth-child(1)",
-          "form > input:nth-of-type(1)",
-          "input",
-        ],
-        frame: null,
-      },
-    });
-    // Verifying that 'The Ultimate Dogfooding Story' is present in the search results.
-    await page.assertPageText({
-      text: "The Ultimate Dogfooding Story",
-    });
-  }
-);
+test(title, details, async ({ page }) => {
+  // Initializing web navigation.
+  await page.goto('https://news.ycombinator.com');
+  // Scrolling down the page to find the search box.
+  await page.scroll({
+    direction: 'DOWN',
+    selector: {
+      element: ['body'],
+      frame: null,
+    },
+  });
+  // Inputting the search query 'mac app for web testing' into the search box to find relevant results.
+  await page.inputText({
+    text: 'mac app for web testing',
+    finalizeWithSubmit: true,
+    selector: {
+      element: [
+        '#hnmain > tbody > tr:nth-of-type(4) > td > center > form > input',
+      ],
+      frame: null,
+    },
+  });
+});
